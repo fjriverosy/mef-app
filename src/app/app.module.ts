c296fbe31cbd23765201847e3d198a407ef0ba23
fjriverosy <friveros@axxezo.com>
2017-09-28 10:43:07 -0300

    first commit: architecture in angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterListComponent } from './public/register-list/register-list.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { BodyComponent } from './common/body/body.component';
+import { LoaderComponent } from './common/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterListComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule
